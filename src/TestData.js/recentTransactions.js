4c14ca32d52eb3e47f66c58d3ca51e2b25b20678
James Nnanyelugo <james@winexviv.com>
2023-10-30 20:36:17 +0100

    Implies beneficiary id to transactions demo data

--- src/TestData.js/recentTransactions.js
+++ src/TestData.js/recentTransactions.js
@@ -7,6 +7,7 @@ export const recentTransactions = [
     Bank: Out,
     amount: -10000,
     id: 1,
+    beneficiary_id: 1,
     date: "2023-10-12",
   },
   {
@@ -14,6 +15,7 @@ export const recentTransactions = [
     Bank: In,
     amount: 5000,
     id: 2,
+    beneficiary_id: 1,
     date: "2023-10-12",
   },
   {
@@ -21,6 +23,7 @@ export const recentTransactions = [
     Bank: Out,
     amount: -18000,
     id: 3,
+    beneficiary_id: 2,
     date: "2023-10-28",
   },
 ];
